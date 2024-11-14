cf90b0e6ee193e0b3467a7c34a7f756c423aae91
nidhish654 <nidhish.shettigar.06@gmail.com>
2024-11-14 17:34:49 +0530

    Transaction CardLimits Cards NavigationBar

---
+++ src/routes/CreditControls/+page.svelte
@@ -0,0 +1,87 @@
+<script>
+    import { BiSolidLeftArrow } from "svelte-icons-pack/bi";
+    import { Icon } from "svelte-icons-pack";
+	import Button from "$lib/components/ui/button/button.svelte";
+    import { Switch } from "$lib/components/ui/switch/index.js";
+    import { onMount } from "svelte";
+    import ToggleSwitch from "$lib/components/ToggleSwitch.svelte";
+    let IisChecked = false;
+    let CisChecked = false;
+    let OisChecked = true;
+    let AisChecked = false;
+    let PisChecked = true;
+
+</script>
+
+<div class="flex items-center justify-between bg-[#D9D9D9] text-[#772035] h-[10dvh] font-bold text-3xl">
+    <div class="ml-4">
+        <a href="/CreditCards">
+        <Icon src={BiSolidLeftArrow}/>
+        </a>
+    </div>
+    <div>Credit Card Limits</div>
+    <div class="mr-4"></div>
+</div>
+
+<div class="flex flex-col justify-center gap-5 items-center h-screen">
+    <div class="bg-[#FDFDFD] w-[70dvh] h-[18dvh] rounded-lg border-2 border-[#772035] flex flex-col">
+        <div class="text-xl font-bold mt-3 ml-6">
+            Domestic
+        </div>
+        <div class="mt-4 flex flex-row justify-between">
+            <div class="ml-6 flex flex-row relative" >
+                <span class="absolute inset-y-0 left-0 flex items-center pl-3">₹</span>
+                <input type="number" class="pl-8 pr-4 py-1 border border-black rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Enter Amount">
+            </div>
+            <div class="mr-6 font-semibold">Daily Limit</div>
+        </div>
+    </div>
+    
+    <div class="bg-[#FDFDFD] text-xl font-bold font-bold w-[70dvh] h-[10dvh] rounded-lg border-2 border-[#772035] flex items-center justify-between">
+        <div class="ml-6">
+            International
+        </div>
+        <div class="mr-6">
+            <ToggleSwitch bind:checked={IisChecked} />
+        </div>
+    </div>
+
+    <div class="bg-[#FDFDFD] text-xl font-bold w-[70dvh] h-[10dvh] rounded-lg border-2 border-[#772035] flex items-center justify-between">
+        <div class="ml-6">
+            Contactless
+        </div>
+        <div class="mr-6">
+            <ToggleSwitch bind:checked={CisChecked} />
+        </div>
+    </div>
+
+    <div class="bg-[#FDFDFD] text-xl font-bold w-[70dvh] h-[10dvh] rounded-lg border-2 border-[#772035] flex items-center justify-between">
+        <div class="ml-6">
+            Online
+        </div>
+        <div class="mr-6">
+            <ToggleSwitch bind:checked={OisChecked} />
+        </div>
+    </div>
+
+    <div class="bg-[#FDFDFD] text-xl font-bold w-[70dvh] h-[10dvh] rounded-lg border-2 border-[#772035] flex items-center justify-between">
+        <div class="ml-6">
+            ATM
+        </div>
+        <div class="mr-6">
+            <ToggleSwitch bind:checked={AisChecked} />
+        </div>
+    </div>
+
+    <div class="bg-[#FDFDFD] text-xl font-bold w-[70dvh] h-[10dvh] rounded-lg border-2 border-[#772035] flex items-center justify-between">
+        <div class="ml-6">
+            POS & QR
+        </div>
+        <div class="mr-6">
+            <ToggleSwitch bind:checked={PisChecked} />
+        </div>
+    </div>
+    <div>
+        <Button variant="outline" class="bg-[#772035] text-[#FDFDFD] hover:bg-[#EEC1CC]">Save Changes</Button>
+    </div>
+</div>
---
+++ src/routes/CreditTransactions/+page.svelte
@@ -0,0 +1,70 @@
+<script>
+    import { BiSolidLeftArrow } from "svelte-icons-pack/bi";
+    import { Icon } from "svelte-icons-pack";
+</script>
+
+<div class="mt-10 flex flex-col">
+    <div class="text-3xl font-bold ml-2 flex flex-row items-center">
+        <div><a href="/CreditCards"> <Icon src={BiSolidLeftArrow}/> </a></div>
+        <div>Transaction History</div>   
+    </div>
+    <div class="flex flex-row border-2 border-black h-[9dvh] items-center font-bold text-xl bg-[#D9D9D9] justify-between mt-5">
+        <div class="ml-14">
+            Date
+        </div>
+        
+        <div class="ml-96">
+            Particulars
+        </div>
+        <div class="flex flex-row gap-6 mr-12">
+            <div class="ml-96 ">
+                Amount
+            </div>
+            <div class="">
+                
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-row border-2 border-black h-[15dvh] items-center font-bold justify-between">
+        <div class="flex flex-col ml-4 items-center justify-center w-[20dvh]">
+            <div>
+                30 OCT
+            </div>
+            <div>
+                2024
+            </div>
+        </div>
+        <div class="w-[90dvh] flex justify-center">
+            UPICC/MOB/429440387632/Smart Point Mangalore/UPI
+        </div>
+        <div class="flex flex-row w-[28dvh] justify-start items-start gap-3 mr-8" >
+            <div class=" w-[45dvh] flex justify-end">
+                ₹ 100.00
+            </div>  
+            <div class="">
+                DR
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-row border-2 border-black h-[15dvh] items-center font-bold justify-between">
+        <div class="flex flex-col ml-4  items-center justify-center w-[20dvh]">
+            <div>
+                30 OCT
+            </div>
+            <div>
+                2024
+            </div>
+        </div>
+        <div class="w-[90dvh] flex justify-center">
+            UPICC/MOB/429440387632/Smart Point Mangalore/UPI
+        </div>
+        <div class="flex flex-row w-[28dvh] justify-start items-start gap-3 mr-8" >
+            <div class=" w-[45dvh] flex justify-end">
+                ₹ 100.00
+            </div>  
+            <div class="">
+                DR
+            </div>
+        </div>
+    </div>
+</div>
